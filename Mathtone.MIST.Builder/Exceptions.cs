2191834e194cfde677c3f96f7a048042ed0c544d
mik3c <mike.carleton@gmail.com>
2016-04-14 14:08:47 -0400

    Made exceptions serializable

--- Mathtone.MIST.Builder/Exceptions.cs
+++ Mathtone.MIST.Builder/Exceptions.cs
@@ -5,25 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Mathtone.MIST {
-
+	[Serializable]
 	public class CannotLocateNotifyTargetException : Exception {
 		public CannotLocateNotifyTargetException(string typeName) :
 			base($"{Resources.CannotLocateNotifyTarget}: {typeName}") {
 		}
 	}
-
+	[Serializable]
 	public class InvalidNotifyTargetException : Exception {
 		public InvalidNotifyTargetException(string methodName) :
 			base(String.Format(Resources.InvalidNotifyTarget, methodName)) {
 		}
 	}
-
+	[Serializable]
 	public class InvalidNotifierException : Exception {
 		public InvalidNotifierException() :
 			base(Resources.NotifyAttributeCannotBeSet) {
 		}
 	}
-
+	[Serializable]
 	public class BuildTaskErrorException : Exception {
 		public BuildTaskErrorException(string typeName, Exception innerException = null) :
 			base(String.Format(Resources.BuildTaskError, typeName), innerException) {
