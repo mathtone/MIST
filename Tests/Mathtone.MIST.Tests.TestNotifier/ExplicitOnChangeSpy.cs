0a1cd288a916971ef1838335df7521b6ff6b12ec
mik3c <mike.carleton@gmail.com>
2016-06-08 00:30:02 -0400

    Merging

---
+++ Tests/Mathtone.MIST.Tests.TestNotifier/ExplicitOnChangeSpy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathtone.MIST.TestNotifier
+{
+    [Notifier]
+    public class ExplicitOnChangeSpy
+    {
+        [SuppressNotify]
+        public int NumberOfNotifications { get; private set; }
+
+        [NotifyTarget]
+        protected void NotifyTargetMethod()
+        {
+            NumberOfNotifications++;
+        }
+
+        [Notify(NotifyMode.OnChange)]
+        public string StringValue { get; set; }
+    }
+}
---
+++ Tests/Mathtone.MIST.Tests.TestNotifier/Explicit_OneArgSpy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathtone.MIST.TestNotifier
+{
+    [Notifier]
+    public class Explicit_OneArgSpy
+    {
+        [SuppressNotify]
+        public List<string> Notifications { get; private set; }
+
+        [NotifyTarget]
+        protected void NotifyTargetMethod(string propertyName)
+        {
+            Notifications.Add(propertyName);
+        }
+
+        [Notify]
+        public string StringValue { get; set; }
+
+        public Explicit_OneArgSpy()
+        {
+            Notifications = new List<string>();
+        }
+    }
+}
