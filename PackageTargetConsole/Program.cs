62624e96fade9a38baaa5c1e6c597883704e1341
mik3c <mike.carleton@gmail.com>
2021-06-03 05:33:45 -0400

    Added netcore 3.5

--- PackageTargetConsole/Program.cs
+++ PackageTargetConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Mathtone.MIST;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PackageTargetConsole {
 
@@ -10,14 +11,15 @@ namespace PackageTargetConsole {
 		public string Name { get; set; }
 
 		static void Main(string[] args) => _ = new Program {
-			Id = "eyyyyy",
-			Name = "therer"
+			Id = "1",
+			Name = "2"
 		};
 	}
 
 	public class NotifierBase {
 
 		[NotifyTarget]
+		[SuppressMessage("Performance", "CA1822:Mark members as static")]
 		protected void OnNotify(string name, object newValue) =>
 			Console.WriteLine($"{name} changed to {newValue}");
 	}
